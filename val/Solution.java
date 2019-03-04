2cf24edf050ce653a643764444995c194298bc47
imagebutton2 <1046509851@qq.com>
2019-03-04 22:27:32 +0800

    删除链表中等于给定值 val 的所有节点

---
+++ val/Solution.java
@@ -0,0 +1,29 @@
+//删除链表中等于给定值 val 的所有节点。
+//输入: 1->2->6->3->4->5->6, val = 6
+        //输出: 1->2->3->4->5
+public class Solution {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return dummyNode.next;
+    }
+}
