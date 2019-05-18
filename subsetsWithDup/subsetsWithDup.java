d3e1905b46565e8b696bd6dfd83d02227498cdc8
imagebutton2 <1046509851@qq.com>
2019-05-18 18:20:19 +0800

    用HashSet去重

--- subsetsWithDup/subsetsWithDup.java
+++ subsetsWithDup/subsetsWithDup.java
@@ -1,8 +1,5 @@
 package My.leetcode.My;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 //给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
 //
 //说明：解集不能包含重复的子集。
@@ -19,29 +16,47 @@ import java.util.List;
 //  [1,2],
 //  []
 //]
+
+import java.util.*;
+
 public class subsetsWithDup {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
         Arrays.sort(nums);
-        List<Integer> levelList = new ArrayList<>();
-        result.add(new ArrayList<>(levelList));
-        subsetsWithDupDFS(result, levelList, 0, nums);
-        return result;
-
-
+        HashSet<List<Integer>> result=new HashSet<>();
+        result.add(new ArrayList<>());
+        generate(0,nums,new ArrayList<>(),result);
+        return new ArrayList<>(result);
     }
-    private void subsetsWithDupDFS(List<List<Integer>> result, List<Integer> levelList, int depth, int[] nums) {
-        for (int i = depth; i < nums.length; i++) {
-            if (i > depth && nums[i - 1] == nums[i]) {
-                continue;
-            }
-            levelList.add(nums[i]);
-            result.add(new ArrayList<>(levelList));
-            subsetsWithDupDFS(result, levelList, i + 1, nums);
-            levelList.remove(levelList.size() - 1);
+
+    private void generate(int i, int[] nums, List<Integer> item,HashSet<List<Integer>> result) {
+        if(i>=nums.length) {
+
+            return;
         }
+        item.add(nums[i]);
+        if(!result.contains(item)){
+            result.add(new ArrayList<>(item));
+        }
+        generate(i+1,nums,item,result);
+        item.remove(item.size()-1);
+        generate(i+1,nums,item,result);
+    }
+
+
+    public static void main(String[] args) {
+        int []nums = new int[]{1,2,2};
+        subsetsWithDup subsetsWithDup=new subsetsWithDup();
+        List<List<Integer>>list= subsetsWithDup.subsetsWithDup(nums);
+        Iterator<List<Integer>> listIterator=list.iterator();
+        while (listIterator.hasNext()){
+            List<Integer> list1=listIterator.next();
+            System.out.print("[");
+            for(Integer i:list1){
+                System.out.print(i+"、");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+
     }
 }
