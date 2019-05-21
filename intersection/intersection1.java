429eff6fa4c49794343df54d051031b98987a6c4
imagebutton2 <1046509851@qq.com>
2019-05-21 18:31:07 +0800

    /**  * 给定两个数组，编写一个函数来计算它们的交集。  * <p>  * 示例 1:  * <p>  * 输入: nums1 = [1,2,2,1], nums2 = [2,2]  * 输出: [2,2]  * <p>  * 示例 2:  * <p>  * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]  * 输出: [4,9]  */

---
+++ intersection/intersection1.java
@@ -0,0 +1,45 @@
+package com.java.myAlgorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ */
+public class intersection1 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            if (!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                map.put(i, map.get(i) + 1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : nums2) {
+            if (map.containsKey(i)) {
+                list.add(i);
+                map.put(i, map.get(i) - 1);
+                if (map.get(i) == 0) {
+                    map.remove(i);
+                }
+            }
+        }
+        int arr[] = new int[list.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
